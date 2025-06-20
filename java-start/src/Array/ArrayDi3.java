d656816ade81d8565c02c8350907877affd04636
KimGyoMin <gyomin0220@gmail.com>
2025-06-20 23:42:49 +0900

    배ì열 연습2

--- java-start/src/Array/ArrayDi3.java
+++ java-start/src/Array/ArrayDi3.java
@@ -7,7 +7,7 @@ public class ArrayDi3 {
         int[][] arr =  {
             {1, 2, 3},
             {4,5,6},
-                {7,8,9}
+            {7,8,9}
         }; //행은 2 열은3
 
         for (int row = 0; row < arr.length; row++) {
