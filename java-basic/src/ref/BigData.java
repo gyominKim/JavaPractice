cc827c90c52d5f316e02f76023d5b8484881b786
gyominKim <9yomin@gmail.com>
2025-08-31 19:43:18 +0900

    ref1

---
+++ java-basic/src/ref/BigData.java
@@ -0,0 +1,6 @@
+package ref;
+
+public class BigData {
+    Data data; //null 참조형이라서
+    int count; //0 숫자여서 0으로
+}
---
+++ java-basic/src/ref/initMain.java
@@ -0,0 +1,10 @@
+package ref;
+
+public class initMain {
+    public static void main(String[] args) {
+        IntiData intiData = new IntiData();
+        System.out.println("value1 = " + intiData.value1);
+        System.out.println("value2 = " + intiData.value2);
+    }
+
+}
