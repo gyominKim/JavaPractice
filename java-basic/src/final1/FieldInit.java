813e8ccf019a8560a009c66dc0961e5d43b9a349
gyominKim <9yomin@gmail.com>
2026-01-15 17:04:40 +0900

    final1

---
+++ java-basic/src/final1/FieldInit.java
@@ -0,0 +1,11 @@
+package final1;
+
+public class FieldInit {
+    static final int CONST_VALUE = 10; //static final 이 붙은 애들 상수 취급
+    final int value = 10;
+
+/*    public FieldInit(int value) {
+        this.value = 10;
+    }*/
+
+}
