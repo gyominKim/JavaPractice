c3a86c7cf697785893d2c20103f6b8a2d93bbcad
gyominKim <9yomin@gmail.com>
2026-02-02 22:16:06 +0900

    poly.ex6

---
+++ java-basic/src/Poly/ex4/AbstractAnimal.java
@@ -0,0 +1,10 @@
+package Poly.ex4;
+
+public abstract class AbstractAnimal {
+
+    public abstract void sound();
+
+    public abstract void move(); // 본인이 가지는 기능 없음 순수추상
+
+
+}
---
+++ java-basic/src/Poly/ex5/InterfaceAnimal.java
@@ -0,0 +1,6 @@
+package Poly.ex5;
+
+public interface InterfaceAnimal {
+    void sound(); //public abstract
+    void move();  //public abstract
+}
